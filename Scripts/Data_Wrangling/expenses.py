fac20bd08f36597537c08e0b06bf0b9dd2bd4c03
bmr83@georgetown.edu <bmr83@georgetown.edu>
2020-05-14 20:47:00 -0700

    Adding final games data, final expenses transformations

--- Scripts/Data_Wrangling/expenses.py
+++ Scripts/Data_Wrangling/expenses.py
@@ -79,4 +79,26 @@ expenses['Total Expenses Int'] = expenses['Total Expenses'].str.replace('$','').
 expenses['footall_expense_pct'] = expenses['Total Football Spending Int']/expenses['Total Expenses Int']
 
 
-expenses.to_csv('../../Data/wrangled-data/expenses.csv')
+#create dfs for opponent 
+shift_cols = ['Total Football Spending Int', 'Total Expenses Int', 'footall_expense_pct']
+
+for col in shift_cols:
+    expenses[col + ' Lagged'] = expenses.groupby('Data')[col].shift()
+
+#select columns of interest
+expenses_fin = expenses[['Data', 'Year', 'Total Football Spending Int Lagged', 
+                         'Total Expenses Int Lagged', 'footall_expense_pct Lagged']]
+
+
+rename_dict = {'Data': 'Team',
+               'Total Expenses Int Lagged': 'Lagged Expenses',
+               'Total Football Spending Int Lagged': 'Lagged Football Spending',
+               'footall_expense_pct Lagged': 'Lagged Football Expenses Pct'
+               }
+
+#rename columns
+expenses_fin = expenses_fin.rename(columns = rename_dict)
+
+
+## save so don't have to 
+expenses_fin.to_csv('../../Data/wrangled-data/expenses.csv')
