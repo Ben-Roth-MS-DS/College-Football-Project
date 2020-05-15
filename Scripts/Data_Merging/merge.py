5ee37634e17f817e5302b98441a67aa0fe212f37
bmr83@georgetown.edu <bmr83@georgetown.edu>
2020-05-15 16:21:45 -0700

    Removing expenses from final df, since ~20 schools do not have readily available data. Keeping other script and data incase want to use in future

--- Scripts/Data_Merging/merge.py
+++ Scripts/Data_Merging/merge.py
@@ -8,32 +8,11 @@ Created on Thu May 14 19:29:39 2020
 
 #import necessary modules
 import pandas as pd
+import numpy as np
 
 #read in datasets
 
 games = pd.read_csv('../../Data/wrangled-data/cfb_win.csv', index_col=0)
-expenses = pd.read_csv('../../Data/wrangled-data/expenses.csv', index_col = 0)
-
-## Final Expenses Transformations ##
-
-#get rid of scientific notation in numeric columns
-cols = [col for col in expenses.columns if not ['Team', 'Year']]
-
-expenses[cols] = expenses[cols].round(0)
-#create expenses data to merge for opponents 
-expenses_opp = expenses.copy()
-
-#rename columns except team and year
-expenses_opp.columns = ['Opponent ' + col if loc > 1 else col for loc, col in enumerate(expenses_opp.columns)]
-
-#rename
-expenses_opp = expenses_opp.rename(columns = {'Team':'Opponent'})
-
-#merge on team
-model_df = pd.merge(games, expenses, on = ['Team', 'Year'], how = 'left')
-
-#merge on opponent
-model_df = pd.merge(model_df, expenses_opp, on = ['Opponent', 'Year'], how = 'left')
 
 
 
